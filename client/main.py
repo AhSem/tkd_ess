2230238ccc561b30d374cc3c63a32ea92d67c521
Sem <chin.sem.chang@gmail.com>
2018-08-12 22:55:36 +0800

    Client Update

--- client/main.py
+++ client/main.py
@@ -1,51 +1,67 @@
+import pickle
+import socket
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
 
-Builder.load_string('''
-<Demo>:
-    cols: 1
+class SettingPopup(Popup):
+    connection_status = ObjectProperty()
+    def __init__(self, arg):
+        super(SettingPopup, self).__init__()
 
-    BoxLayout:
-        Button:
-			id: "Bluehead"
-            size_hint_x: 0.5
-            text: 'Tap(Swipe)Head 2'
-			background_color: 0,2,3,1
-	
-        Button:
-			id: "Redhead"
-            size_hint_x: 0.5
-            text: 'Tap(Swipe)Head'
-			background_color: 2,0,0,1
+    def on_host_changed(self, textinput):
+        self.host = textinput.text
+        
+    def on_port_changed(self, textinput):
+        self.port = textinput.text
+    
+    def on_connect_btn_pressed(self, button):
+        app = App.get_running_app()
+        app.root.try_connect()
+    
+    def on_close_btn_pressed(self, button):
+        self.dismiss()
 
-    BoxLayout:
-        Button:
-			id: "BlueBody"
-            size_hint_x: 0.5
-            text: 'Tap(Swipe)Body 3'
-			background_color: 0,2,3,1
+class ClientRoot(BoxLayout):
+    action_bar_title = ObjectProperty()
+    chung_score = ObjectProperty()
+    hong_score = ObjectProperty()
 
-        Button:
-			id: "RedBody"
-            size_hint_x: 0.5
-            text: 'Tap(Swipe)Body'
-			background_color: 2,0,0,1
+    def __init__(self, **kwargs):
+        super(ClientRoot, self).__init__(**kwargs)
+        self.popup = SettingPopup(self)
 
-''')
+    def open_setting_modal(self):
+        self.popup.open()
 
+    def try_connect(self, *args):
+        try:
+            server = (self.popup.host, int(self.popup.port))
+            self.s = socket.socket()
+            self.s.connect(server)
+            self.action_bar_title.title = self.popup.connection_status.text = 'Connected'
+        except:
+            self.action_bar_title.title = self.popup.connection_status.text = 'Disconnected'
 
-class Demo(GridLayout):
-    pass
+    def score(self, side, point):
+        message = {side: point}
+        print('Sending: ', message)
+        try:
+            self.s.sendall(pickle.dumps(message))
+        except:
+            self.action_bar_title.title = self.popup.connection_status.text = 'Disconnected'
 
+class EssClient(App):
 
-class DemoApp(App):
     def build(self):
-        return Demo()
-
+        self.root = ClientRoot()
+        return self.root
 
 if __name__ == '__main__':
-    DemoApp().run()
-
-	
+    EssClient().run() 
